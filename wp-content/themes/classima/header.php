f8d723fe387178f59ad2881b92698b99d691e74c
Ngan Ly <lythanhngan167@gmail.com>
2022-11-14 18:21:10 +0700

    fix banner image

--- wp-content/themes/classima/header.php
+++ wp-content/themes/classima/header.php
@@ -14,22 +14,22 @@ namespace radiustheme\Classima;
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<?php wp_head(); ?>
-	
+
 </head>
 
 <body <?php body_class(); ?>>
 	<?php do_action( 'wp_body_open' );?>
-		
+
 	<div id="page" class="site">
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'classima' ); ?></a>
 		<?php get_template_part( 'template-parts/content', 'menu' ); ?>
-			 
+
 		<div id="content" class="site-content">
-			
+
 		<div class="banner-main">
 	<div class="container">
         <a href="#">
-        <img src="http://192.168.1.7/simdaiviet/wp-content/uploads/2022/10/banenrphongthuy-1.png" alt="" style="
+        <img src="http://simdaiviet.com/wp-content/uploads/2022/10/banenrphongthuy-1.png" alt="" style="
     padding-top: 10px;
 ">
 		</a>
@@ -37,10 +37,10 @@ namespace radiustheme\Classima;
 </div>
 
 
-	
+
 <div class="container">
 	<div class="banner-content">
-		
+
 		<?php if ( RDTheme::$has_breadcrumb ): ?>
 		<div class="main-breadcrumb"><?php Helper::the_breadcrumb(); ?></div>
 		<?php endif; ?>
@@ -50,12 +50,12 @@ namespace radiustheme\Classima;
 
 
 			<!-- <?php get_template_part('template-parts/content', 'banner'); ?> -->
-			
-			
-			
+
+
+
 			<!-- <div class="banner-main">
 	<div class="container">
-        
+
         <img src="http://192.168.1.5/khosim/wp-content/uploads/2022/10/banenrphongthuy.png" alt="" style="
     padding-top: 10px;
 ">
