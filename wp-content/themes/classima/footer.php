f6fcc71a540c8f575f660be9bff163e6a4c0612b
Nguyễn Mạnh Cường <nguyenmanhcuong722001@gmail.com>
2022-11-25 13:39:03 +0700

    css icon phone+mobie

--- wp-content/themes/classima/footer.php
+++ wp-content/themes/classima/footer.php
@@ -13,28 +13,22 @@ namespace radiustheme\Classima;
 </div><!-- #page -->
 <?php wp_footer();?>
 
-<div class="hotline-phone-ring-wrap">
-	<div class="hotline-phone-ring">
-		<div class="hotline-phone-ring-circle"></div>
-		<div class="hotline-phone-ring-circle-fill"></div>
-		<div class="hotline-phone-ring-img-circle">
-		<a href="tel:0971565656" class="pps-btn-img">
-			<img src="https://nguyenhung.net/wp-content/uploads/2019/05/icon-call-nh.png" alt="Gọi điện thoại" width="50">
-		</a>
-		</div>
-	</div>
-	<div class="hotline-bar">
-		<a href="tel:0971565656">
-			<span class="text-hotline">0971.56.56.56</span>
-		</a>
-	</div>
-	
-</div>
-<!-- them icon cho zalo -->
-<div class="sitechatzalo" id="zalo">
-<a id="zalonum" href="https://zalo.me/0971565656" target="_blank"><span class="iczalo">&nbsp;</span> Chat Zalo</a>
-</div>
 
+<!-- Hiển thị icon trên Máy Tính -->
+<ul class="sharecs-pc-net-bar">
+ <li class="zalo">
+	 <a href="https://zalo.me/0971565656" target="_blank" rel="nofollow"></a>
+ </li>
+</ul>
+<!-- Hiển thị icon trên điện thoại -->
+<ul class="sharecs-mobile-net-bar">
+ <li class="zalo">
+	 <a href="https://zalo.me/0971565656" target="_blank" rel="nofollow"></a>
+ </li>
+ <li class="hotline">
+	 <a href="tel:0971565656" target="_blank" rel="nofollow"></a>
+ </li>
+ </ul>
 
 </div>
 </div>
